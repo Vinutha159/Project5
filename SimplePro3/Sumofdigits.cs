99f0cdc6097865fc4cb1b21a736ed1a13742b049
Vinutha B S <vbs@catchpoint.com>
2019-09-25 21:31:13 +0530

    testing purposes

---
+++ SimplePro3/Sumofdigits.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace SimplePro3
+{
+    public class Sumofdigits
+    {
+        static void Main(string[] args)
+        {
+            int n, sum = 0, m;
+            Console.Write("Enter a number: ");
+            n = int.Parse(Console.ReadLine());
+            while (n > 0)
+            {
+                m = n % 10;
+                sum = sum + m;
+                n = n / 10;
+            }
+            Console.Write("Sum is= " + sum);
+        }
+    }
+}
